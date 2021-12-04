39efd46fe722e57dba37440dcf6d777606db1835
lynxthecat <lynx.light0@gmail.com>
2021-12-04 18:09:14 +0000

    Update sqm-autorate.sh

--- sqm-autorate.sh
+++ sqm-autorate.sh
@@ -8,16 +8,14 @@
 
 debug=1
 
-enable_verbose_output=1 # enable (1) or disable (0) output monitoring lines showing bandwidth changes
+enable_verbose_output=0 # enable (1) or disable (0) output monitoring lines showing bandwidth changes
 
 ul_if=wan # upload interface
 dl_if=veth-lan # download interface
 
-max_ul_rate=35000 # maximum bandwidth for upload
-min_ul_rate=25000 # minimum bandwidth for upload
+base_ul_rate=30000 # steady state bandwidth for upload
 
-max_dl_rate=70000 # maximum bandwidth for download
-min_dl_rate=25000 # minimum bandwidth for download
+base_dl_rate=30000 # steady state bandwidth for download
 
 tick_duration=1 # seconds to wait between ticks
 
@@ -26,7 +24,7 @@ alpha_RTT_decrease=0.9 # how rapidly baseline RTT is allowed to decrease
 
 rate_adjust_RTT_spike=0.05 # how rapidly to reduce bandwidth upon detection of bufferbloat
 rate_adjust_load_high=0.005 # how rapidly to increase bandwidth upon high load detected
-rate_adjust_load_low=0.0025 # how rapidly to decrease bandwidth upon low load detected
+rate_adjust_load_low=0.0025 # how rapidly to return to base rate upon low load detected
 
 load_thresh=0.5 # % of currently set bandwidth for detecting high load
 
@@ -34,27 +32,27 @@ max_delta_RTT=10 # increase from baseline RTT for detection of bufferbloat
 
 # verify these are correct using 'cat /sys/class/...'
 case "${dl_if}" in
-    \veth*) 
+    \veth*)
         rx_bytes_path="/sys/class/net/${dl_if}/statistics/tx_bytes"
-    	;;
-    \ifb*) 
+        ;;
+    \ifb*)
         rx_bytes_path="/sys/class/net/${dl_if}/statistics/tx_bytes"
-    	;;
-    *) 
+        ;;
+    *)
         rx_bytes_path="/sys/class/net/${dl_if}/statistics/rx_bytes"
-	;;
+        ;;
 esac
 
 case "${ul_if}" in
-    \veth*) 
+    \veth*)
         tx_bytes_path="/sys/class/net/${ul_if}/statistics/rx_bytes"
-    	;;
-    \ifb*) 
+        ;;
+    \ifb*)
         tx_bytes_path="/sys/class/net/${ul_if}/statistics/rx_bytes"
-    	;;
-    *) 
+        ;;
+    *)
         tx_bytes_path="/sys/class/net/${ul_if}/statistics/tx_bytes"
-	;;
+        ;;
 esac
 
 if [ "$debug" ] ; then
@@ -92,50 +90,51 @@ get_next_shaper_rate() {
     local cur_delta_RTT
     local cur_max_delta_RTT
     local cur_rate
-    local cur_rate_adjust_RTT_spike
-    local cur_max_rate
-    local cur_min_rate
+    local cur_base_rate
     local cur_load
     local cur_load_thresh
+    local cur_rate_adjust_RTT_spike
     local cur_rate_adjust_load_high
     local cur_rate_adjust_load_low
-    
+
     local next_rate
-    
+    local cur_rate_decayed_down
+    local cur_rate_decayed_up
+
     cur_delta_RTT=$1
     cur_max_delta_RTT=$2
     cur_rate=$3
-    cur_rate_adjust_RTT_spike=$4
-    cur_max_rate=$5
-    cur_min_rate=$6
-    cur_load=$7
-    cur_load_thresh=$8
-    cur_rate_adjust_load_high=$9
-    cur_rate_adjust_load_low=${10}
+    cur_base_rate=$4
+    cur_load=$5
+    cur_load_thresh=$6
+    cur_rate_adjust_RTT_spike=$7
+    cur_rate_adjust_load_high=$8
+    cur_rate_adjust_load_low=$9
 
-
-	# in case of supra-threshold RTT spikes decrease the rate unconditionally
-	if awk "BEGIN {exit !($cur_delta_RTT >= $cur_max_delta_RTT)}"; then
-	    next_rate=$( call_awk "int(${cur_rate} - ${cur_rate_adjust_RTT_spike} * (${cur_max_rate} - ${cur_min_rate}) )" )
+        # in case of supra-threshold RTT spikes decrease the rate unconditionally
+        if awk "BEGIN {exit !($cur_delta_RTT >= $cur_max_delta_RTT)}"; then
+            next_rate=$( call_awk "int(${cur_rate} * ( 1 - ${cur_rate_adjust_RTT_spike}) )" )
         else
-	    # ... otherwise take the current load into account
-	    # high load, so we would like to increase the rate
-	    if awk "BEGIN {exit !($cur_load >= $cur_load_thresh)}"; then
-                next_rate=$( call_awk "int(${cur_rate} + ${cur_rate_adjust_load_high} * (${cur_max_rate} - ${cur_min_rate}) )" )
-	    else
-	        # low load gently decrease the rate again
-		        next_rate=$( call_awk "int(${cur_rate} - ${cur_rate_adjust_load_low} * (${cur_max_rate} - ${cur_min_rate}) )" )
-        fi
-	fi
+            # ... otherwise take the current load into account
+            # high load, so we would like to increase the rate
+            if awk "BEGIN {exit !($cur_load >= $cur_load_thresh)}"; then
+                next_rate=$( call_awk "int(${cur_rate} * ( 1 + ${cur_rate_adjust_load_high}) )" )
+            fi
+            # low load, so determine whether to decay down, decay up, or set as base rate
+            cur_rate_decayed_down=$( call_awk "int(${cur_rate} * ( 1 - ${cur_rate_adjust_load_low}) )" )
+            cur_rate_decayed_up=$( call_awk "int(${cur_rate} * ( 1 + ${cur_rate_adjust_load_low}) )" )
 
-	# make sure to only return rates between cur_min_rate and cur_max_rate
-        if awk "BEGIN {exit !($next_rate < $cur_min_rate)}"; then
-            next_rate=$cur_min_rate;
+            if awk "BEGIN {exit !($cur_rate_decayed_down > $base_rate)}"; then
+                # low load gently decrease to steady state rate
+                next_rate=$cur_rate_decayed_down
+            elif awk "BEGIN {exit !($cur_rate_decayed_up < $base_rate)}"; then
+                # low load gently increase to steady state rate
+                next_rate=$cur_rate_decayed_up
+            else
+                next_rate=$base_rate
+        fi
         fi
 
-        if awk "BEGIN {exit !($next_rate > $cur_max_rate)}"; then
-            next_rate=$cur_max_rate;
-        fi
         echo "${next_rate}"
 }
 
@@ -145,19 +144,17 @@ function update_rates {
         cur_rx_bytes=$(cat $rx_bytes_path)
         cur_tx_bytes=$(cat $tx_bytes_path)
         t_cur_bytes=$(date +%s.%N)
-        
+
         rx_load=$( call_awk "(8/1000)*(${cur_rx_bytes} - ${prev_rx_bytes}) / (${t_cur_bytes} - ${t_prev_bytes}) * (1/${cur_dl_rate}) " )
-   	tx_load=$( call_awk "(8/1000)*(${cur_tx_bytes} - ${prev_tx_bytes}) / (${t_cur_bytes} - ${t_prev_bytes}) * (1/${cur_ul_rate}) " )
+        tx_load=$( call_awk "(8/1000)*(${cur_tx_bytes} - ${prev_tx_bytes}) / (${t_cur_bytes} - ${t_prev_bytes}) * (1/${cur_ul_rate}) " )
 
         t_prev_bytes=$t_cur_bytes
         prev_rx_bytes=$cur_rx_bytes
         prev_tx_bytes=$cur_tx_bytes
 
-	# calculate the next rate for dl and ul
-	cur_dl_rate=$( get_next_shaper_rate "$delta_RTT" "$max_delta_RTT" "$cur_dl_rate" "$rate_adjust_RTT_spike" "$max_dl_rate" "$min_dl_rate" "$rx_load" "$load_thresh" "$rate_adjust_load_high" "$rate_adjust_load_low" )
-	cur_ul_rate=$( get_next_shaper_rate "$delta_RTT" "$max_delta_RTT" "$cur_ul_rate" "$rate_adjust_RTT_spike" "$max_ul_rate" "$min_ul_rate" "$tx_load" "$load_thresh" "$rate_adjust_load_high" "$rate_adjust_load_low" )
-
-
+        # calculate the next rate for dl and ul
+        cur_dl_rate=$( get_next_shaper_rate "$delta_RTT" "$max_delta_RTT" "$cur_dl_rate" "$base_dl_rate" "$rx_load" "$load_thresh" "$rate_adjust_RTT_spike" "$rate_adjust_load_high" "$rate_adjust_load_low")
+        cur_ul_rate=$( get_next_shaper_rate "$delta_RTT" "$max_delta_RTT" "$cur_ul_rate" "$base_ul_rate" "$tx_load" "$load_thresh" "$rate_adjust_RTT_spike" "$rate_adjust_load_high" "$rate_adjust_load_low")
 
         if [ $enable_verbose_output -eq 1 ]; then
                 printf "%s;%14.2f;%14.2f;%14.2f;%14.2f;%14.2f;%14.2f;%14.2f;\n" $( date "+%Y%m%dT%H%M%S.%N" ) $rx_load $tx_load $baseline_RTT $RTT $delta_RTT $cur_dl_rate $cur_ul_rate
@@ -170,20 +167,20 @@ get_baseline_RTT() {
     local last_baseline_RTT
     local cur_alpha_RTT_increase
     local cur_alpha_RTT_decrease
-    
+
     local cur_baseline_RTT
-    
+
     cur_RTT=$1
     cur_delta_RTT=$2
     last_baseline_RTT=$3
     cur_alpha_RTT_increase=$4
     cur_alpha_RTT_decrease=$5
         if awk "BEGIN {exit !($cur_delta_RTT >= 0)}"; then
-		cur_baseline_RTT=$( call_awk "( 1 - ${cur_alpha_RTT_increase} ) * ${last_baseline_RTT} + ${cur_alpha_RTT_increase} * ${cur_RTT} " )
+                cur_baseline_RTT=$( call_awk "( 1 - ${cur_alpha_RTT_increase} ) * ${last_baseline_RTT} + ${cur_alpha_RTT_increase} * ${cur_RTT} " )
         else
-		cur_baseline_RTT=$( call_awk "( 1 - ${cur_alpha_RTT_decrease} ) * ${last_baseline_RTT} + ${cur_alpha_RTT_decrease} * ${cur_RTT} " )
+                cur_baseline_RTT=$( call_awk "( 1 - ${cur_alpha_RTT_decrease} ) * ${last_baseline_RTT} + ${cur_alpha_RTT_decrease} * ${cur_RTT} " )
         fi
-    
+
     echo "${cur_baseline_RTT}"
 }
 
@@ -215,26 +212,26 @@ fi
 while true
 do
         t_start=$(date +%s.%N)
-	get_RTT
-	delta_RTT=$( call_awk "${RTT} - ${baseline_RTT}" )
-	baseline_RTT=$( get_baseline_RTT "$RTT" "$delta_RTT" "$baseline_RTT" "$alpha_RTT_increase" "$alpha_RTT_decrease" )
+        get_RTT
+        delta_RTT=$( call_awk "${RTT} - ${baseline_RTT}" )
+        baseline_RTT=$( get_baseline_RTT "$RTT" "$delta_RTT" "$baseline_RTT" "$alpha_RTT_increase" "$alpha_RTT_decrease" )
         update_rates
 
-	# only fire up tc if there are rates to change...
+        # only fire up tc if there are rates to change...
         if [ "$last_dl_rate" -ne "$cur_dl_rate" ] ; then
-    	    #echo "tc qdisc change root dev ${dl_if} cake bandwidth ${cur_dl_rate}Kbit"
-    	    tc qdisc change root dev ${dl_if} cake bandwidth ${cur_dl_rate}Kbit
+            #echo "tc qdisc change root dev ${dl_if} cake bandwidth ${cur_dl_rate}Kbit"
+            tc qdisc change root dev ${dl_if} cake bandwidth ${cur_dl_rate}Kbit
         fi
         if [ "$last_ul_rate" -ne "$cur_ul_rate" ] ; then
-    	    #echo "tc qdisc change root dev ${ul_if} cake bandwidth ${cur_ul_rate}Kbit"
-    	    tc qdisc change root dev ${ul_if} cake bandwidth ${cur_ul_rate}Kbit
+            #echo "tc qdisc change root dev ${ul_if} cake bandwidth ${cur_ul_rate}Kbit"
+            tc qdisc change root dev ${ul_if} cake bandwidth ${cur_ul_rate}Kbit
         fi
         # remember the last rates
-	last_dl_rate=$cur_dl_rate
-	last_ul_rate=$cur_ul_rate
+        last_dl_rate=$cur_dl_rate
+        last_ul_rate=$cur_ul_rate
 
         t_end=$(date +%s.%N)
-	sleep_duration=$( call_awk "${tick_duration} - ${t_end} + ${t_start}" )
+        sleep_duration=$( call_awk "${tick_duration} - ${t_end} + ${t_start}" )
         if awk "BEGIN {exit !($sleep_duration > 0)}"; then
                 sleep $sleep_duration
         fi
