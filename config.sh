df7c02c6ee79e21f54ec902cf75f9f213e2a025c
lynxthecat <lynx.light0@gmail.com>
2022-03-19 18:45:06 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,12 +5,12 @@
 # config.sh is a part of CAKE-autorate
 # CAKE-autorate automatically adjusts bandwidth for CAKE in dependence on detected load and RTT
 
-# inspired by @moeller0 (OpenWrt forum)
-# initial sh implementation by @Lynx (OpenWrt forum)
-# requires packages: iputils-ping, coreutils-date and coreutils-sleep
+# Author: @Lynx (OpenWrt forum)
+# Inspiration taken from: @moeller0 (OpenWrt forum)
 
-output_processing_stats=0 # enable (1) or disable (0) output monitoring lines showing processing stats
+output_processing_stats=1 # enable (1) or disable (0) output monitoring lines showing processing stats
 output_cake_changes=0     # enable (1) or disable (0) output monitoring lines showing cake bandwidth changes
+debug=0			  # enable (1) or disable (0) out of debug lines
 
 ul_if=wan # upload interface
 dl_if=veth-lan # download interface
@@ -23,7 +23,7 @@ reflector_ping_interval=0.1 # (seconds)
 reflectors=("1.1.1.1" "1.0.0.1" "8.8.8.8" "8.8.4.4")
 no_reflectors=${#reflectors[@]}
 
-delay_thr=15 # extent of RTT increase to classify as a delay
+delay_thr=25 # extent of RTT increase to classify as a delay
 
 min_dl_rate=20000 # minimum bandwidth for download
 base_dl_rate=25000 # steady state bandwidth for download
@@ -38,19 +38,19 @@ max_ul_rate=35000 # maximum bandwidth for upload
 bufferbloat_detection_window=4 # number of delay samples to retain in detection window
 bufferbloat_detection_thr=2    # number of delayed samples for bufferbloat detection
 
-alpha_baseline_increase=1 # how rapidly baseline RTT is allowed to increase (integer /1000)
-alpha_baseline_decrease=900 # how rapidly baseline RTT is allowed to decrease (integer /1000)
+alpha_baseline_increase=0.001 # how rapidly baseline RTT is allowed to increase
+alpha_baseline_decrease=0.9   # how rapidly baseline RTT is allowed to decrease
 
-rate_adjust_bufferbloat=150 # how rapidly to reduce bandwidth upon detection of bufferbloat (integer /1000)
-rate_adjust_load_high=10 # how rapidly to increase bandwidth upon high load detected (integer /1000)
-rate_adjust_load_low=25 # how rapidly to return to base rate upon low load detected (integer /1000)
+rate_adjust_bufferbloat=0.85 # how rapidly to reduce bandwidth upon detection of bufferbloat 
+rate_adjust_load_high=1.01   # how rapidly to increase bandwidth upon high load detected 
+rate_adjust_load_low=0.975   # how rapidly to return to base rate upon low load detected 
 
-high_load_thr=50 # % of currently set bandwidth for detecting high load (integer /100)
+high_load_thr=0.75 # % of currently set bandwidth for detecting high load
 
 bufferbloat_refractory_period=300 # (milliseconds)
 decay_refractory_period=5000 # (milliseconds)
 
-sustained_base_rate_sleep_thr=1000 # time threshold to put pingers to sleep on sustained ul and dl base rate (seconds)
+sustained_base_rate_sleep_thr=60 # time threshold to put pingers to sleep on sustained ul and dl base rate (seconds)
 
 # verify these are correct using 'cat /sys/class/...'
 case "${dl_if}" in
@@ -81,5 +81,3 @@ if (( $debug )) ; then
     echo "rx_bytes_path: $rx_bytes_path"
     echo "tx_bytes_path: $tx_bytes_path"
 fi
-
-
