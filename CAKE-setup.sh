e6a0ecb5234ef294c75c70f73aaf5f912d30f083
Rich Brown <richb.hanover@gmail.com>
2022-08-16 07:39:04 -0400

    Pleasing appearance of the installer flow; now debug it

--- CAKE-setup.sh
+++ CAKE-setup.sh
@@ -3,7 +3,9 @@
 # See https://github.com/lynxthecat/sqm-autorate for details
 # https://www.shellcheck.net/ is your friend
 
-SRC_DIR="https://raw.githubusercontent.com/lynxthecat/CAKE-autorate/main/"
+#SRC_DIR="https://raw.githubusercontent.com/lynxthecat/CAKE-autorate/main/"
+SRC_DIR="https://raw.githubusercontent.com/richb-hanover/CAKE-autorate/setup-script/"
+DOC_URL="https://github.com/lynxthecat/CAKE-autorate#installation-on-openwrt"
 
 # Retrieve required packages
 # opkg update
@@ -16,49 +18,48 @@ cd /root/ || exit
 # create the CAKE-autorate directory if it's not present
 [[ -d CAKE-autorate ]] || mkdir CAKE-autorate
 
+printf "Installing CAKE-autorate in /root/CAKE-autorate...\n"
+
 # cd into it
 cd CAKE-autorate/ || exit
-pwd
 
 # rm the main script and fetch a fresh copy
 [[ -f CAKE-autorate.sh ]] && rm CAKE-autorate.sh
 wget -q "$SRC_DIR"CAKE-autorate.sh
-echo "Retrieved CAKE-autorate.sh"
 
 # Check if the configuration script exists, and ask whether to keep it
 
+editmsg=$(printf "\nNow edit the CAKE-config.sh file as described in:\n   $DOC_URL")
+
 if [ -f CAKE-config.sh ]; then
-	echo "Previous configuration present - keep it? [Y/n]"
+	printf "Previous configuration present - keep it? [Y/n] "
 	read keepIt
 	if [ "$keepIt" == "N" ] || [ "$keepIt" == "n" ]; then
 		rm ./CAKE-config.sh
 		wget -q "$SRC_DIR"CAKE-config.sh
-		echo "Retrieved CAKE-config.sh"
-		echo "Now edit the CAKE-config.sh file as described in:" 
-		echo " https://github.com/lynxthecat/CAKE-autorate#installation-on-openwrt"
 	else
-		echo "Using saved configuration"
+		editmsg="Using prior configuration"
 	fi
 else 
-	wget "$SRC_DIR"CAKE-config.sh 
-	echo "Retrieved CAKE-config.sh (really)"
-	echo "Now edit the CAKE-config.sh file as described in:" 
-	echo " https://github.com/lynxthecat/CAKE-autorate#installation-on-openwrt"	
+	wget -q "$SRC_DIR"CAKE-config.sh 
 fi
 # make both .sh files executable
 chmod +x *.sh
+
+# Tell how to handle the config file - use old, or edit the new one
+printf "$editmsg \n"
 
 # Also copy over the service file but DO NOT ACTIVATE IT
 # cd into the directory and remove the previous file
 cd /etc/init.d || exit
 [[ -f cake-autorate ]] && rm cake-autorate
 wget -q "$SRC_DIR"cake-autorate
-echo "Retrieved cake-autorate service script"
 chmod +x cake-autorate
 
-# Go back to the beginning
-cd /root/CAKE-autorate || exit
-echo " "
-echo "CAKE-autorate installation successful"
-echo "You can start the software manually with 'cd /root/CAKE-autorate; bash ./CAKE-autorate.sh'"
-echo "   or run as a service with 'service cake-autorate enable;service cake-autorate start'"
+printf "\n"
+printf "`grep CAKE_autorate /root/CAKE-autorate/CAKE-config.sh` successfully installed but not yet running\n"
+printf "\n"
+printf "Start the software manually with:\n"
+printf "   cd /root/CAKE-autorate; bash ./CAKE-autorate.sh\n"
+printf "Run as a service with:\n"
+printf "   service cake-autorate enable;service cake-autorate start\n"
