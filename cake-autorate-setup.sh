51d2a4d1cc50e8ae611313e3e7b17135a0dfae75
lynxthecat <lynx.light0@gmail.com>
2023-01-18 18:16:54 +0000

    Update installation script

--- cake-autorate-setup.sh
+++ cake-autorate-setup.sh
@@ -7,7 +7,9 @@ SRC_DIR="https://raw.githubusercontent.com/lynxthecat/cake-autorate/main/"
 DOC_URL="https://github.com/lynxthecat/CAKE-autorate#installation-on-openwrt"
 
 # Retrieve required packages
+printf "Running opkg update to update package lists:\n"
 opkg update
+printf "Installing bash, iputils-ping and fping packages:\n"
 opkg install bash iputils-ping fping
 
 # Set up CAKE-autorate files
@@ -26,21 +28,21 @@ cd cake-autorate/ || exit
 [[ -f cake-autorate.sh ]] && rm cake-autorate.sh
 wget -q "$SRC_DIR"cake-autorate.sh
 
-# Check if the configuration script exists, and ask whether to keep it
+# Check if a configuration file exists, and ask whether to keep it
 
-editmsg="\nNow edit the cake-autorate_config.sh file as described in:\n   $DOC_URL"
+editmsg="\nNow edit the cake-autorate_config.primary.sh file as described in:\n   $DOC_URL"
 
-if [ -f cake-autorate_config.sh ]; then
+if [ -f cake-autorate_config.primary.sh ]; then
 	printf "Previous configuration present - keep it? [Y/n] "
 	read keepIt
 	if [ "$keepIt" == "N" ] || [ "$keepIt" == "n" ]; then
-		rm ./cake-autorate_config.sh
-		wget -q "$SRC_DIR"cake-autorate_config.sh
+		rm ./cake-autorate_config.primary.sh
+		wget -q "$SRC_DIR"cake-autorate_config.primary.sh
 	else
 		editmsg="Using prior configuration"
 	fi
 else 
-	wget -q "$SRC_DIR"cake-autorate_config.sh 
+	wget -q "$SRC_DIR"cake-autorate_config.primary.sh 
 fi
 
 # make both .sh files executable
@@ -52,14 +54,14 @@ printf "$editmsg \n"
 # Also copy over the service file but DO NOT ACTIVATE IT
 # cd into the directory and remove the previous file
 wget -q "$SRC_DIR"cake-autorate_launcher.sh
+chmod +x cake-autorate_launcher.sh
 cd /etc/init.d || exit
 [[ -f cake-autorate ]] && rm cake-autorate
 wget -q "$SRC_DIR"cake-autorate
 chmod +x cake-autorate
 
-printf "\n`grep cake_autorate_version /root/cake-autorate/cake-autorate_config.sh` successfully installed, but not yet running\n\n"
+printf "\n`grep cake_autorate_version /root/cake-autorate/cake-autorate_config.primary.sh` successfully installed, but not yet running\n\n"
 printf "Start the software manually with:\n"
-printf "   cd /root/cake-autorate; bash ./cake-autorate.sh\n"
+printf "   cd /root/cake-autorate; ./cake-autorate.sh\n"
 printf "Run as a service with:\n"
 printf "   service cake-autorate enable; service cake-autorate start\n\n"
-printf "And don't forget to disable output logging if running as a service!\n"
