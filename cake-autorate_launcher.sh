412e2ca1010dc982f5ebfd3bc8d99d9f5894234b
rany <ranygh@riseup.net>
2023-02-10 19:40:21 +0200

    Fix cake-autorate_launcher robustness

--- cake-autorate_launcher.sh
+++ cake-autorate_launcher.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# shellcheck source=cake-autorate_lib.sh
-source /root/cake-autorate/cake-autorate_lib.sh
 cake_instances=(/root/cake-autorate/cake-autorate_config*sh)
+cake_instance_pids=()
+cake_instance_list=()
 
 trap kill_cake_instances INT TERM EXIT
 
@@ -14,19 +14,15 @@ kill_cake_instances()
 
 	for ((cake_instance=0; cake_instance<${#cake_instances[@]}; cake_instance++))
 	do
-		kill "${cake_instance_pids[${cake_instance}]}"
-		wait "${cake_instance_pids[${cake_instance}]}"
+		kill "${cake_instance_pids[${cake_instance}]}" 2>/dev/null || true
 	done
-	kill "${sleep_pid}"
 }
 
 for cake_instance in "${cake_instances[@]}"
 do
-	/root/cake-autorate/cake-autorate.sh "$cake_instance"&
-	cake_instance_pids+=($!)
+	/root/cake-autorate/cake-autorate.sh "$cake_instance" &
+	cake_instance_pids+=(${!})
 	cake_instance_list+=("${cake_instance}")
 done
 
-sleep_inf&
-sleep_pid+=($!)
 wait
