lynxthecat/cake-autorate

commit 17607d7e90e2cd68b1f3fe214b9561a760a255bb

Replace costly regex with printf
Performance enhancement. See: https://github.com/lynxthecat/cake-autorate/issues/154

cake-autorate.sh
@@ -1071,14 +1071,12 @@ concurrent_read_integer()
 	do
 		read -r value < "${path}"
 
-		# Verify value is a positive or negative integer 
-		# 1st capture group (optional): negative sign
-		# 2nd capture group (optional): leading zeros
-		# 3rd capture group (not optional): numeric sequence 
-		if [[ ${value} =~ ^([-])?([0]+)?([0-9]+)$ ]]; then
+		# printf '%.0f' is used here to sanitize unsigned integers:
+		# - it removes any leading zeros whilst preserving the sign; and
+		# - it returns false if ${value} is not a number
+		if printf -v sanitized_value '%.0f' ${value} 2>/dev/null; then
 
-			# Strip out any leading zeros and employ arithmetic context
-			value=$((${BASH_REMATCH[1]}${BASH_REMATCH[3]}))
+			value=${sanitized_value}
 			true
 			return
 

commit b6299cfb3cbaf3be4e08144ce74a2905ddc40a1e

Fix bug introduced by 17607d7e90e2cd68b1f3fe214b9561a760a255bb
* If value is empty, we will think the number is 0. This fixes
  the issue.

cake-autorate.sh
@@ -1074,7 +1074,7 @@ concurrent_read_integer()
 		# printf '%.0f' is used here to sanitize unsigned integers:
 		# - it removes any leading zeros whilst preserving the sign; and
 		# - it returns false if ${value} is not a number
-		if printf -v sanitized_value '%.0f' ${value} 2>/dev/null; then
+		if printf -v sanitized_value '%.0f' "${value:-unset}" 2>/dev/null; then
 
 			value=${sanitized_value}
 			true