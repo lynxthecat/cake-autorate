68c23970ade7faa225b31a8bcf93b281877308da
lynxthecat <lynx.light0@gmail.com>
2023-02-16 11:51:01 +0000

    Provide more guidance for config overrides

--- cake-autorate_config.primary.sh
+++ cake-autorate_config.primary.sh
@@ -22,4 +22,13 @@ min_ul_shaper_rate_kbps=5000  # minimum bandwidth for upload (Kbit/s)
 base_ul_shaper_rate_kbps=20000 # steady state bandwidth for upload (KBit/s)
 max_ul_shaper_rate_kbps=35000  # maximum bandwidth for upload (Kbit/s)
 
+# *** OVERRIDES ***
+
+### See cake-autorate_defaults.sh for additional configuration options
+### that can be set in this configuration file to override the defaults.
+### Place any such overrides below this line.
+
+
+# *** DO NOT EDIT BELOW THIS LINE ***
+
 config_file_check="cake-autorate"
