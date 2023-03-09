7affe95cefdb40ca5f96eb48546f59a0f9a10d2f
rany <ranygh@riseup.net>
2023-03-10 01:18:28 +0200

    Update cake-autorate_lib.sh
    Co-authored-by: lynxthecat <lynx.light0@gmail.com>

--- cake-autorate_lib.sh
+++ cake-autorate_lib.sh
@@ -17,7 +17,7 @@ sleep_s()
 	#
 	# Due to bash's loadable sleep module not being available
 	# For OpenWRT and most embedded systems, use of the bash
-	# read command with a timeout which performs roughly
+	# read command with a timeout offers performance that is
 	# as good as bash's sleep module.
 	#
 	# For benchmarks, check the following links:
