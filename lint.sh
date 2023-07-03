277f9f236b867a149085ecde6b40849d4815b54c
rany <ranygh@riseup.net>
2023-07-03 14:41:25 +0300

    Switch to shellcheckrc and drop lint.sh script
    Signed-off-by: rany <ranygh@riseup.net>

--- lint.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export SHELLCHECK_OPTS="-e SC2244 -e SC2312 -e SC2086 -e SC2261 -e SC1083 -e SC2093"
-find . -name '*.sh' ! -name '*config*.sh' ! -name 'defaults.sh' -print0 | exec xargs -0 shellcheck -x -o all
