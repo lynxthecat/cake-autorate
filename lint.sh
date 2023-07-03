1f6bdf3de0194225c8eebca74deebcae9eb26165
rany <ranygh@riseup.net>
2023-07-03 13:21:05 +0300

    Ignore ShellCheck errors that do not apply for our script
    We should ignore SC2086 because have disabled file globbing
    and SC2261 because it raises false positives.
    
    Signed-off-by: rany <ranygh@riseup.net>

--- lint.sh
+++ lint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-export SHELLCHECK_OPTS="-e SC2244 -e SC2312"
+export SHELLCHECK_OPTS="-e SC2244 -e SC2312 -e SC2086 -e SC2261"
 find . -name '*.sh' ! -name '*config*.sh' ! -name 'defaults.sh' -print0 | exec xargs -0 shellcheck -x -o all
