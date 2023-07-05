7dbfb416a4cbc7a446c4800d692b3fd529e40642
lynxthecat <lynx.light0@gmail.com>
2023-07-05 09:31:17 +0100

    Change (dl/ul)_delay_thr_ms to floats

--- defaults.sh
+++ defaults.sh
@@ -78,8 +78,8 @@ reflector_ping_interval_s=0.3 # (seconds, e.g. 0.2s or 2s)
 # delay threshold in ms is the extent of OWD increase to classify as a delay
 # these are automatically adjusted based on maximum on the wire packet size
 # (adjustment significant at sub 12Mbit/s rates, else negligible)
-dl_delay_thr_ms=30 # (milliseconds)
-ul_delay_thr_ms=30 # (milliseconds)
+dl_delay_thr_ms=30.0 # (milliseconds)
+ul_delay_thr_ms=30.0 # (milliseconds)
 
 # Set either of the below to 0 to adjust one direction only
 # or alternatively set both to 0 to simply use cake-autorate to monitor a connection
