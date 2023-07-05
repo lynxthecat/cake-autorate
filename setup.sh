0eb959e69b7f4207af97eec26fb2c651b55a51fc
rany <ranygh@riseup.net>
2023-07-05 13:28:18 +0300

    Add optional argument(s) to setup.sh
    Closes #209
    
    Signed-off-by: rany <ranygh@riseup.net>

--- setup.sh
+++ setup.sh
@@ -12,7 +12,7 @@ DEPENDENCIES="jsonfilter uclient-fetch tar grep"
 
 # Set up remote locations and branch
 BRANCH="master"
-REPOSITORY="${CAKE_AUTORATE_REPO:-lynxthecat/cake-autorate}"
+REPOSITORY="${CAKE_AUTORATE_REPO:-${1-lynxthecat/cake-autorate}}"
 SRC_DIR="https://github.com/${REPOSITORY}/archive/"
 API_URL="https://api.github.com/repos/${REPOSITORY}/commits/${BRANCH}"
 DOC_URL="https://github.com/${REPOSITORY}/tree/${BRANCH}#installation-on-openwrt"
