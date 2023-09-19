706ea424cf931c7b828f1b1751106d605f3c892b
lynxthecat <lynx.light0@gmail.com>
2023-09-19 13:45:37 +0100

    Update Example_Starlink_config.sh
    Signed-off-by: rany <ranygh@riseup.net>

--- Example_Starlink_config.sh
+++ Example_Starlink_config.sh
@@ -22,11 +22,21 @@ min_ul_shaper_rate_kbps=2000  # minimum bandwidth for upload (Kbit/s)
 base_ul_shaper_rate_kbps=10000 # steady state bandwidth for upload (KBit/s)
 max_ul_shaper_rate_kbps=30000  # maximum bandwidth for upload (Kbit/s)
 
-# delay threshold in ms is the extent of OWD increase to classify as a delay
+# *** OVERRIDES ***
+
+### See defaults.sh for additional configuration options
+### that can be set in this configuration file to override the defaults.
+### Place any such overrides below this line.
+
+# owd delta threshold in ms is the extent of OWD increase to classify as a delay
 # these are automatically adjusted based on maximum on the wire packet size
-# (adjustment significant at sub 12Mbit/s rates, else negligible)  
-dl_delay_thr_ms=40 # (milliseconds)
-ul_delay_thr_ms=40 # (milliseconds)
+# (adjustment significant at sub 12Mbit/s rates, else negligible)
+dl_owd_delta_thr_ms=40.0 # (milliseconds)
+ul_owd_delta_thr_ms=40.0 # (milliseconds)
+
+# average owd delta threshold in ms at which maximum adjust_down_bufferbloat is applied
+dl_avg_owd_delta_thr_ms=80.0 # (milliseconds)
+ul_avg_owd_delta_thr_ms=80.0 # (milliseconds)
 
 # Starlink satellite switch (sss) compensation options
 sss_compensation=1
@@ -34,9 +44,3 @@ sss_compensation=1
 #sss_times_s=("12.0" "27.0" "42.0" "57.0")
 #sss_compensation_pre_duration_ms=300
 #sss_compensation_post_duration_ms=200
-
-# *** OVERRIDES ***
-
-### See defaults.sh for additional configuration options
-### that can be set in this configuration file to override the defaults.
-### Place any such overrides below this line.
