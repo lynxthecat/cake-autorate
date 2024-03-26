lynxthecat/cake-autorate

commit ce9ce4dd507023b5246d296942c65cf63c0e9b1e

Use buffered log file writes

cake-autorate.sh
@@ -295,7 +295,7 @@ flush_log_fd()
 	while read -r -t 0 -u "${log_fd}"
 	do
 		read -r -u "${log_fd}" log_line
-		printf '%s\n' "${log_line}" >> "${log_file_path}"
+		printf '%s\n' "${log_line}" >&"${log_file_fd}"
 	done
 }
 
@@ -327,39 +327,45 @@ maintain_log_file()
 
 	get_log_file_size_bytes
 
-	while read -r -u "${log_fd}" log_line
+	while true
 	do
+		exec {log_file_fd}> "${log_file_path}"
 
-		printf '%s\n' "${log_line}" >> "${log_file_path}"
+		while read -r -N "${log_file_buffer_size_B}" -u "${log_fd}" log_chunk
+		do
+			printf '%s' "${log_chunk}" >&"${log_file_fd}"
 
-		# Verify log file size < configured maximum
-		# The following two lines with costly call to 'du':
-		# 	read log_file_size_bytes< <(du -b ${log_file_path}/cake-autorate.log)
-		# 	log_file_size_bytes=${log_file_size_bytes//[!0-9]/}
-		# can be more efficiently handled with this line:
-		((log_file_size_bytes=log_file_size_bytes+${#log_line}+1))
+			((log_file_size_bytes+=log_file_buffer_size_B))
 
-		# Verify log file time < configured maximum
-		if (( (${EPOCHREALTIME/./}-t_log_file_start_us) > log_file_max_time_us ))
+			# Verify log file time < configured maximum
+			if (( (${EPOCHREALTIME/./}-t_log_file_start_us) > log_file_max_time_us ))
+			then
+
+				log_msg "DEBUG" "log file maximum time: ${log_file_max_time_mins} minutes has elapsed so flushing and rotating log file."
+				break
+			# Verify log file size < configured maximum
+			elif (( log_file_size_bytes > log_file_max_size_bytes ))
+			then
+				log_file_size_KB=$((log_file_size_bytes/1024))
+				log_msg "DEBUG" "log file size: ${log_file_size_KB} KB has exceeded configured maximum: ${log_file_max_size_KB} KB so flushing and rotating log file."
+				break
+			elif (( reset_log_file_signalled ))
+			then
+				log_msg "DEBUG" "received log file reset signal so flushing and resetting log file."
+				break
+			fi
+		done
+
+		flush_log_fd
+		if ((reset_log_file_signalled))
 		then
-
-			log_msg "DEBUG" "log file maximum time: ${log_file_max_time_mins} minutes has elapsed so flushing and rotating log file."
-			flush_log_fd
-			rotate_log_file
-		elif (( log_file_size_bytes > log_file_max_size_bytes ))
-		then
-			log_file_size_KB=$((log_file_size_bytes/1024))
-			log_msg "DEBUG" "log file size: ${log_file_size_KB} KB has exceeded configured maximum: ${log_file_max_size_KB} KB so flushing and rotating log file."
-			flush_log_fd
-			rotate_log_file
-		elif (( reset_log_file_signalled ))
-		then
-			log_msg "DEBUG" "received log file reset signal so flushing and resetting log file."
-			flush_log_fd
 			reset_log_file
 			reset_log_file_signalled=0
+		else
+			rotate_log_file
 		fi
 
+		exec {log_file_fd}>&-
 	done
 }
 

defaults.sh
@@ -111,6 +111,8 @@ startup_wait_s=0.0 # number of seconds to wait on startup (e.g. to wait for thin
 
 # *** ADVANCED CONFIGURATION OPTIONS ***
 
+log_file_buffer_size_B=512 # log file buffer size in bytes
+
 log_file_export_compress=1 # compress log file exports using gzip and append .gz to export filename
 
 ### In multi-homed setups, it is mandatory to use either ping_extra_args

commit a117a797481acf938fbf5d34270812ed4d2100bb

Fix header printing bug introduced by ce9ce4d

cake-autorate.sh
@@ -158,19 +158,19 @@ print_headers()
 	log_msg "DEBUG" "Starting: ${FUNCNAME[0]} with PID: ${BASHPID}"
 
 	header="DATA_HEADER; LOG_DATETIME; LOG_TIMESTAMP; PROC_TIME_US; DL_ACHIEVED_RATE_KBPS; UL_ACHIEVED_RATE_KBPS; DL_LOAD_PERCENT; UL_LOAD_PERCENT; ICMP_TIMESTAMP; REFLECTOR; SEQUENCE; DL_OWD_BASELINE; DL_OWD_US; DL_OWD_DELTA_EWMA_US; DL_OWD_DELTA_US; DL_ADJ_DELAY_THR; UL_OWD_BASELINE; UL_OWD_US; UL_OWD_DELTA_EWMA_US; UL_OWD_DELTA_US; UL_ADJ_DELAY_THR; DL_SUM_DELAYS; DL_AVG_OWD_DELTA_US; DL_ADJ_AVG_OWD_DELTA_THR_US; UL_SUM_DELAYS; UL_AVG_OWD_DELTA_US; UL_ADJ_AVG_OWD_DELTA_THR_US; DL_LOAD_CONDITION; UL_LOAD_CONDITION; CAKE_DL_RATE_KBPS; CAKE_UL_RATE_KBPS"
-	((log_to_file)) && printf '%s\n' "${header}" >> "${log_file_path}"
+	((log_to_file)) && printf '%s\n' "${header}" >&${log_file_fd}
 	((terminal)) && printf '%s\n' "${header}"
 
 	header="LOAD_HEADER; LOG_DATETIME; LOG_TIMESTAMP; PROC_TIME_US; DL_ACHIEVED_RATE_KBPS; UL_ACHIEVED_RATE_KBPS; CAKE_DL_RATE_KBPS; CAKE_UL_RATE_KBPS"
-	((log_to_file)) && printf '%s\n' "${header}" >> "${log_file_path}"
+	((log_to_file)) && printf '%s\n' "${header}" >&${log_file_fd}
 	((terminal)) && printf '%s\n' "${header}"
 
 	header="REFLECTOR_HEADER; LOG_DATETIME; LOG_TIMESTAMP; PROC_TIME_US; REFLECTOR; MIN_SUM_OWD_BASELINES_US; SUM_OWD_BASELINES_US; SUM_OWD_BASELINES_DELTA_US; SUM_OWD_BASELINES_DELTA_THR_US; MIN_DL_DELTA_EWMA_US; DL_DELTA_EWMA_US; DL_DELTA_EWMA_DELTA_US; DL_DELTA_EWMA_DELTA_THR; MIN_UL_DELTA_EWMA_US; UL_DELTA_EWMA_US; UL_DELTA_EWMA_DELTA_US; UL_DELTA_EWMA_DELTA_THR"
-	((log_to_file)) && printf '%s\n' "${header}" >> "${log_file_path}"
+	((log_to_file)) && printf '%s\n' "${header}" >&${log_file_fd}
 	((terminal)) && printf '%s\n' "${header}"
 
 	header="SUMMARY_HEADER; LOG_DATETIME; LOG_TIMESTAMP; DL_ACHIEVED_RATE_KBPS; UL_ACHIEVED_RATE_KBPS; DL_SUM_DELAYS; UL_SUM_DELAYS; DL_AVG_OWD_DELTA_US; UL_AVG_OWD_DELTA_US; DL_LOAD_CONDITION; UL_LOAD_CONDITION; CAKE_DL_RATE_KBPS; CAKE_UL_RATE_KBPS"
-	((log_to_file)) && printf '%s\n' "${header}" >> "${log_file_path}"
+	((log_to_file)) && printf '%s\n' "${header}" >&${log_file_fd}
 	((terminal)) && printf '%s\n' "${header}"
 }
 
@@ -185,10 +185,6 @@ rotate_log_file()
 		cat "${log_file_path}" > ${log_file_path}.old
 		true > ${log_file_path}
 	fi
-
-	((output_processing_stats)) && print_headers
-	t_log_file_start_us=${EPOCHREALTIME/.}
-	get_log_file_size_bytes
 }
 
 reset_log_file()
@@ -197,10 +193,6 @@ reset_log_file()
 
 	rm -f "${log_file_path}.old"
 	true > ${log_file_path}
-
-	((output_processing_stats)) && print_headers
-	t_log_file_start_us=${EPOCHREALTIME/.}
-	get_log_file_size_bytes
 }
 
 generate_log_file_scripts()
@@ -320,13 +312,15 @@ maintain_log_file()
 	log_msg "DEBUG" "Starting: ${FUNCNAME[0]} with PID: ${BASHPID}"
 
 	reset_log_file_signalled=0
-	t_log_file_start_us=${EPOCHREALTIME/.}
-
-	get_log_file_size_bytes
 
 	while true
 	do
 		exec {log_file_fd}> ${log_file_path}
+
+		print_headers
+		get_log_file_size_bytes
+
+		t_log_file_start_us=${EPOCHREALTIME/.}
 
 		while read -r -N "${log_file_buffer_size_B}" -u "${log_fd}" log_chunk
 		do
@@ -1008,7 +1002,7 @@ else
 	log_file_path=/var/log/cake-autorate${instance_id:+.${instance_id}}.log
 fi
 
-rotate_log_file # rotate here to force header prints at top of log file
+rotate_log_file
 
 # save stderr fd, redirect stderr to intercept_stderr
 # intercept_stderr sends stderr to log_msg and exits cake-autorate