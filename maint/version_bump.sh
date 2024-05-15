lynxthecat/cake-autorate

commit bad280ce4a25274e3231c34366bc3b7b85520d50

Update version branch link in README.md
See #244

maint/version_bump.sh
@@ -18,6 +18,8 @@ version=${1:?}
 is_latest=${2:-1}
 EDITOR=${EDITOR:-nano}
 
+version_major=${version:0:3}
+
 if asker "Would you like to create a new changelog entry? (y/n) "
 then
 	cur_date=$(date -I)
@@ -35,7 +37,8 @@ fi
 
 if ((is_latest))
 then
-	if sed -E 's|(<span id=\"version\">)[^\<]+(</span>)|\1'"${version}"'\2|' -i README.md
+	if sed -E 's|(<span id=\"version\">)[^\<]+(</span>)|\1'"${version}"'\2|' -i README.md && \
+	   sed -E 's|\[v[^\<]+( branch[^\<]+tree\/v)([^\<]+)|\[v'${version_major}'\1'${version_major}'|' -i README.md
 	then
 		echo Latest cake autorate version updated in README.md
 	fi

commit d4b767642b2f36e6f50b32ef3647ccec88326d36

Fix getting major version in version_bump.sh
Fixes: bad280ce4a2527 ("Update version branch link in README.md")
Signed-off-by: rany <ranygh@riseup.net>

maint/version_bump.sh
@@ -17,8 +17,7 @@ git reset --hard
 version=${1:?}
 is_latest=${2:-1}
 EDITOR=${EDITOR:-nano}
-
-version_major=${version:0:3}
+version_major=$(cut -d. -f1,2 <<<"${version}")
 
 if asker "Would you like to create a new changelog entry? (y/n) "
 then