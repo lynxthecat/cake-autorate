lynxthecat/cake-autorate

commit bad280ce4a25274e3231c34366bc3b7b85520d50

Update version branch link in README.md
See #244

maint/version_bump.sh
@@ -18,6 +18,8 @@ version=${1:?}
 is_latest=${2:-1}
 EDITOR=${EDITOR:-nano}
 
+version_major=${version:0:3}
+
 if asker "Would you like to create a new changelog entry? (y/n) "
 then
 	cur_date=$(date -I)
@@ -35,7 +37,8 @@ fi
 
 if ((is_latest))
 then
-	if sed -E 's|(<span id=\"version\">)[^\<]+(</span>)|\1'"${version}"'\2|' -i README.md
+	if sed -E 's|(<span id=\"version\">)[^\<]+(</span>)|\1'"${version}"'\2|' -i README.md && \
+	   sed -E 's|\[v[^\<]+( branch[^\<]+tree\/v)([^\<]+)|\[v'${version_major}'\1'${version_major}'|' -i README.md
 	then
 		echo Latest cake autorate version updated in README.md
 	fi

commit d26ea4592a1792b39597ecb280d3f5256fdd64a8

Fix newly added regex in version_bump.sh
It was wrong and worked by chance, seems like there was confusion
about this being a markdown doc vs html.

Fixes: bad280ce4a2527 ("Update version branch link in README.md")
Signed-off-by: rany <ranygh@riseup.net>

maint/version_bump.sh
@@ -37,8 +37,8 @@ fi
 
 if ((is_latest))
 then
-	if sed -E 's|(<span id=\"version\">)[^\<]+(</span>)|\1'"${version}"'\2|' -i README.md && \
-	   sed -E 's|\[v[^\<]+( branch[^\<]+tree\/v)([^\<]+)|\[v'${version_major}'\1'${version_major}'|' -i README.md
+	if sed -E -e 's|(<span id=\"version\">)[^\<]+(</span>)|\1'"${version}"'\2|' \
+			  -e 's|\[v[^\<]+( branch[^\<]+tree\/v)([^\)]+)|\[v'${version_major}'\1'${version_major}'|' -i README.md
 	then
 		echo Latest cake autorate version updated in README.md
 	fi